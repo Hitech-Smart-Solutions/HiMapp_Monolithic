9a3ec3827e39d1f57059586fdc9fc66c9b35b1de
parth.jani <parth.jani@DESKTOP-MN2UCVQ>
2026-07-29 15:40:43 +0530

    Data Type Changes

--- src/Modules/Execution/Execution.Application/Features/Activities/Commands/ActivityCommands.cs
+++ src/Modules/Execution/Execution.Application/Features/Activities/Commands/ActivityCommands.cs
@@ -3,5 +3,5 @@ using MediatR;
 namespace Himapp.Execution.Application.Features.Activities.Commands;
 
 public sealed record CreateActivityCommand(int CompanyID, string ActivityName,int UOMID,int CreateBy,int LastModifiedBy) : IRequest<ActivityDto>;
-public sealed record UpdateActivityCommand(long Id, string ActivityName, int UOMID, int LastModifiedBy) : IRequest<ActivityDto?>;
-public sealed record DeleteActivityCommand(long Id) : IRequest<bool>;
+public sealed record UpdateActivityCommand(int Id, string ActivityName, int UOMID, int LastModifiedBy) : IRequest<ActivityDto?>;
+public sealed record DeleteActivityCommand(int Id) : IRequest<bool>;
--- src/Modules/Execution/Execution.Application/Features/Activities/Queries/ActivityQueries.cs
+++ src/Modules/Execution/Execution.Application/Features/Activities/Queries/ActivityQueries.cs
@@ -3,4 +3,4 @@ using MediatR;
 namespace Himapp.Execution.Application.Features.Activities.Queries;
 
 public sealed record GetAllActivitiesQuery : IRequest<IReadOnlyCollection<ActivityDto>>;
-public sealed record GetActivityByIdQuery(long Id) : IRequest<ActivityDto?>;
+public sealed record GetActivityByIdQuery(int Id) : IRequest<ActivityDto?>;
