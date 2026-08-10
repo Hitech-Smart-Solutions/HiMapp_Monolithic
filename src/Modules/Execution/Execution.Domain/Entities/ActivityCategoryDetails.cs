2cd317bcae4c6a0748dbd0d7986c73960ad35477
parth.jani <parth.jani@DESKTOP-MN2UCVQ>
2026-08-10 10:45:34 +0530

    New 2 Masters Added

---
+++ src/Modules/Execution/Execution.Domain/Entities/ActivityCategoryDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Himapp.Execution.Domain.Entities
+{
+    public sealed class ActivityCategoryDetails
+    {
+        public int ID { get; set; }
+        public Guid UniqueID { get; set; }
+        public int ActivityID { get; set; }
+        public int CategoryTypeID { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public int? LastModifiedBy { get; set; }
+        public DateTimeOffset LastModifiedDate { get; set; }
+    }
+}
