cd7186b50062fff080f18af1f07cdf2fda8d0917
CoderKing2705 <soumy.oza@hitechsmartsolutions.com>
2026-09-23 17:35:58 +0530

    Added POST API endpoint for CheckingTransactionLock.

---
+++ src/Modules/Execution/Execution.Application/Features/DailyProgress/Models/CheckTransactionLockModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Himapp.Execution.Application.Features.DailyProgress.Models
+{
+    public class CheckTransactionLockModel
+    {
+        public int ProgramID { get; set; }
+        public int ProjectID { get; set; }
+        public int SectionID { get; set; }
+        public DateTime ReportDate { get; set; }
+    }
+}
