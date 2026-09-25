bd786f73b865a8e597dbab5f81a28a77825bdde1
CoderKing2705 <soumy.oza@hitechsmartsolutions.com>
2026-09-25 11:53:28 +0530

    Added Get API endpoint for GetLockOpenRequestByIdAndProgramId.

---
+++ src/Modules/Execution/Execution.Application/Features/LockRequest/Models/LockOpenRequestByIDModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Himapp.Execution.Application.Features.LockRequest.Models
+{
+    public sealed record LockOpenRequestByIDModel
+    {
+        public int ID { get; init; }
+
+        public Guid UniqueID { get; init; }
+
+        public string? RequestCode { get; init; }
+
+        public DateTime RequestDate { get; init; }
+
+        public string? Remarks { get; init; }
+
+        public int CompanyID { get; init; }
+
+        public int ProjectID { get; init; }
+
+        public int ProgramID { get; init; }
+
+        public short StateID { get; init; }
+
+        public bool IsActive { get; init; }
+
+        public int CreatedBy { get; init; }
+
+        public DateTime CreatedDate { get; init; }
+
+        public int? LastModifiedBy { get; init; }
+
+        public DateTime? LastModifiedDate { get; init; }
+
+        public IReadOnlyCollection<LockOpenRequestDetailModel> Details { get; init; }
+            = Array.Empty<LockOpenRequestDetailModel>();
+    }
+}
